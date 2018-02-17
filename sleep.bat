6ac709ac1f7e259c613c865993b0f1c7e397868c
cryptohypocrisy <36382502+cryptohypocrisy@users.noreply.github.com>
2018-02-16 22:29:23 -0500

    Add sleep.bat

---
+++ sleep.bat
@@ -0,0 +1,26 @@
+@echo off
+echo.
+
+SET /P choice=Sleep, Shutdown or Abort (sl/sh/a)? 
+IF %choice% NEQ a (
+	SET minutes=...
+	SET /P minutes=In how many minutes? 
+	SET /A seconds="60*minutes"
+)
+
+echo.
+
+IF %choice%==sl (
+	psshutdown -d -t %seconds%
+	echo.
+)
+IF %choice%==sh (
+	psshutdown -k -t %seconds%
+	echo.
+)
+if %choice%==a (
+	psshutdown -a
+	echo.
+)
+
+pause
